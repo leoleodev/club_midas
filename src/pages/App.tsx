08153321c1c42d7044407a691322ea1513a6b77a
Leonardo <leonardoj.m.4@gmail.com>
2022-12-14 22:02:55 -0300

    commit inicial

---
+++ src/pages/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return(
+    <div>
+      <h1>Iniciando Projeto Club MIDAS</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Velit numquam dolorum, consequuntur illo, unde fugiat omnis
+          perferendis sunt sint exercitationem animi at id nemo nulla
+           alias veritatis tenetur ea aperiam.</p>
+    </div>
+  );
+
+}
+
+export default App
