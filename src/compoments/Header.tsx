ba234c4fec2b181f07aba0112d6bed9179a37018
Leonardo <leonardoj.m.4@gmail.com>
2022-12-14 22:30:14 -0300

    testes iniciais

---
+++ src/compoments/Header.tsx
@@ -0,0 +1,6 @@
+
+export function Header(){
+    return(
+        <h1>Club MIDAS</h1>
+    );
+}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,15 +1,11 @@
+import { Header } from "../compoments/Header";
 
 function App() {
 
   return(
-    <div>
-      <h1>Iniciando Projeto Club MIDAS</h1>
-
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-         Velit numquam dolorum, consequuntur illo, unde fugiat omnis
-          perferendis sunt sint exercitationem animi at id nemo nulla
-           alias veritatis tenetur ea aperiam.</p>
-    </div>
+    <>
+      <Header />
+    </>
   );
 
 }
