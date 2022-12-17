4c4324623823d30705f5627c7930c5b5420da9d9
Leonardo <leonardoj.m.4@gmail.com>
2022-12-17 19:39:48 -0300

    add lib styled components

--- src/compoments/Header.tsx
+++ src/compoments/Header.tsx
@@ -1,9 +1,29 @@
 
+import styled from 'styled-components';
+
+export const Titles = styled.div`
+
+    header{
+        width: 100%;
+        height: 15vh;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        background: teal;
+    }
+    
+`;
+
 export function Header(){
     return(
-        <>
-            <h1 className="logo">Club MIDAS</h1>
-            <h2 className="subTitle logo">MIDAS Logística</h2>
-        </>
+        <Titles>
+            <header>
+                <h1 className="logo">Club MIDAS</h1>
+                <h2 className="subTitle logo">MIDAS Logística</h2>
+            </header>
+        </Titles>
     );
 }
