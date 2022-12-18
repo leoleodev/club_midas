41fe890140b977bb8bdc5ea4b3cec50e87509fcb
Leonardo <leonardoj.m.4@gmail.com>
2022-12-17 22:59:24 -0300

    component main in progress

--- src/compoments/Header.tsx
+++ src/compoments/Header.tsx
@@ -12,7 +12,14 @@ export const Titles = styled.div`
         align-items: center;
         justify-content: center;
 
-        /* background: teal; */
+    }
+    .widthConatinerHeader{
+        width: 80%;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
     }
     .logo{
         color: var(--midasYellow);
@@ -22,6 +29,12 @@ export const Titles = styled.div`
         font-size: 1.2rem;
         letter-spacing: 0.09rem;
     }
+
+    @media(max-width: 600px){
+        .widthConatinerHeader{
+            width: 90%;
+        }
+    }
     
 `;
 
@@ -29,8 +42,10 @@ export function Header(){
     return(
         <Titles>
             <header>
-                <h1 className="logo">Club MIDAS</h1>
-                <h2 className="subTitle logo">MIDAS Logística</h2>
+                <div className="widthConatinerHeader">
+                    <h1 className="logo">Club MIDAS</h1>
+                    <h2 className="subTitle logo">MIDAS Logística</h2>
+                </div>
             </header>
         </Titles>
     );
--- src/compoments/Main.tsx
+++ src/compoments/Main.tsx
@@ -6,7 +6,76 @@ export const Content = styled.div`
 main{
     width: 100%;
     height: 80vh;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    h4{
+        color: var(--midasWhite);
+        font-size: 1rem;
+        font-weight: 400;
+        letter-spacing: 0.09rem;
+    }
+    
 }
+.widthConatinerMain{
+    width: 80%;
+    height: 100%;    
+
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+
+}
+
+.section1{
+    width: 100%;
+    height: 100%;
+
+    padding: 1rem 0 1rem 1rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    h2{
+        height: 70%;
+        padding: 0 4rem 0 2rem;
+        text-align: right;
+        border-right: 1px solid var(--midasWhite);
+
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        justify-content: center;
+        
+        color: var(--midasYellow);
+
+        span{
+            color: var(--midasWhite);
+        }
+
+    }
+}
+
+.section2{
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    form{
+        width: 80%;
+        height: 90%;       
+
+        background: var(--midasYellow);
+    }
+
+}
+
 
 `;
 
@@ -14,7 +83,22 @@ export function Main(){
     return(
         <Content>
             <main>
-                
+                <div className="widthConatinerMain">
+                    <section className='section1'>
+                        <h2>
+                            Seja membro do <span>Club MIDAS</span> e obtenha descontos de 
+                            combustível e outros benefícos
+                        </h2>
+                        
+                    </section>
+                    
+                    <section className="section2">
+
+                        <form action=""></form>
+
+                    </section>
+                </div>
+                <h4>saiba mais</h4>                
             </main>
         </Content>
     )
